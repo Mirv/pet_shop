c0e4dbad6bb2c5f9582442e9afccc996134e4a4b
Mirv <reski.raptor@gmail.com>
2018-04-30 14:32:49 +0000

    setup pet name to not null allowed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180430141013) do
+ActiveRecord::Schema.define(version: 20180430143041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20180430141013) do
   end
 
   create_table "pets", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",                            null: false
     t.text     "description"
     t.integer  "pet_category_id"
     t.integer  "location_id"
